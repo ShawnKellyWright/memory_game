0ee6d01f259373e041b78c555ffeb4ef318eeeab
Shawn Wright <shawncodewright@gmail.com>
2020-12-07 21:42:52 -0600

    added new functions

--- app.js
+++ app.js
@@ -53,5 +53,41 @@ document.addEventListener('DOMContentLoaded', () => {
     ]
 })
 
-const grid = document.querySelector('grid')
+const grid = document.querySelector('.grid')
+var cardsChosen = []
+var cardsChosenId = []
 
+//create Board
+function createBoard() {
+    for (let i = 0; i < cardArray.length; i++) {
+        var card = document.createElement('img')
+        card.setAttribute('src', 'images/blank.png')
+        card.setAttribute('data-id', i)
+        // card.addEventListener('click', flipcard)
+        grid.appendChild(card)
+    }
+}
+
+//Check for matches
+function checkForMatch() {
+    var cards = document.querySelectorAll('img')
+    const optionOneId = cardsChosenId[0]
+    const optionTwoId = cardsChosenId[1]
+    if (cardsChosen[0] === cardsChosen[1]) {
+        alert('You found a match')
+        cards[optionOneId].setAttribute('src')
+    }
+}
+
+//Flip your card
+function flipcard() {
+    var cardId = this.getAttribute('data-id')
+    cardsChosen.push(cardArray[cardId].name)
+    cardsChosenId.push(cardId)
+    this.setAttribute('src', cardArray[cardId].img)
+    if (cardsChosen.length === 2) {
+        setTimeout(checkForMatch, 500)
+    }
+}
+
+createBoard()
