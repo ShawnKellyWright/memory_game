22bd7d589ba1dd7fd3409324afd8b603d8aa5710
Shawn Wright <shawncodewright@gmail.com>
2020-12-07 21:09:14 -0600

    Initial commit

---
+++ app.js
@@ -0,0 +1,54 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    // Card options
+    const cardArray = [
+        {
+            name: 'beets',
+            img: 'images/beet.svg'
+        },
+        {
+            name: 'beets',
+            img: 'images/beet.svg'
+        },
+        {
+            name: 'andy',
+            img: 'images/andy1_100.png'
+        },
+        {
+            name: 'andy',
+            img: 'images/andy1_100.png'
+        },
+        {
+            name: 'michael',
+            img: 'images/michael_scott_100.png'
+        },
+        {
+            name: 'michael',
+            img: 'images/michael_scott_100.png'
+        },
+        {
+            name: 'prisonMike',
+            img: 'images/prison_Mike_100.png'
+        },
+        {
+            name: 'prisonMike',
+            img: 'images/prison_Mike_100.png'
+        },
+        {
+            name: 'cylon',
+            img: 'images/cylon.svg'
+        },
+        {
+            name: 'cylon',
+            img: 'images/cylon.svg'
+        },
+        {
+            name: 'bear',
+            img: 'images/bear.svg'
+        },
+        {
+            name: 'bear',
+            img: 'images/bear.svg'
+        },
+    ]
+})
